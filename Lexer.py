2048aecf84e644cb7a42505ec0bc9ec65049ef2b
Javier Bustillo <javier.bustillo@upr.edu>
2018-03-28 15:52:21 -0400

    Started work on lexer

---
+++ Lexer.py
@@ -0,0 +1,17 @@
+import lex
+
+#Initializing tokens
+#TODO Add rest of tokens
+tokens = {
+    'BLOCKCHAIN'
+    'ADD'
+    'MINE'
+    'ASSIGN'
+    'LET'
+    'ID'
+    'PRINT'
+    'JSON'
+}
+
+#Declare action for each token
+#TODO Declare actions for tokens
