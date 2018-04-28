ded99f20f2844ebaa848c3d29547839ba2404e89
javierbustillo <javier.bustillo@upr.edu>
2018-04-27 20:02:27 -0400

    Started work on new_data function in blockchain

--- Blockchain.py
+++ Blockchain.py
@@ -1,19 +1,25 @@
 class Blockchain(object):
-    def __init__(self, p):
+    def __init__(self, types, param):
         self.chain = []
         self.current_data = []
-        self.datatypes = p
+        self.datatypes = types
+        self.parameters = param
+
 
     def new_block(self):
         # Creates a new Block and adds it to the chain
         pass
 
-    def new_data(self, p):
+    def new_data(self, data):
         # Adds new data to the list of data
         # Receives p, an array of data sent by the user.
         # This method checks each element of p if their data types fit  the defined data types
         # If they fit, then the data is added to the list of current_data
-        pass
+        t = ()
+        for datum in data:
+            t.__add__(datum)
+        self.current_data.append(t)
+
 
     @staticmethod
     def hash(block):
